39fdd344e093a887bd899e1726081f1db7fe200c
Yash Gadle <yashgadle@Yashs-MacBook-Air.local>
2025-06-15 16:54:37 +0530

    refactor a bit

--- src/board.ts
+++ src/board.ts
@@ -1,5 +1,6 @@
 import { Piece, type Position } from "./piece";
 import coordinatesToNotations from "./utils/coordinates-to-notations";
+import { makeDefaultState } from "./utils/default-board-state";
 import { parseFEN, isLowerCase, type parsedFEN } from "./utils/parse-fen";
 
 export type State = Array<Array<Cell>>;
@@ -29,88 +30,9 @@ export class Board {
       );
       return;
     }
-    this.state = [
-      [
-        new Cell(new Piece("r", "b", [0, 0]), "w"),
-        new Cell(new Piece("n", "b", [0, 1]), "b"),
-        new Cell(new Piece("b", "b", [0, 2]), "w"),
-        new Cell(new Piece("q", "b", [0, 3]), "b"),
-        new Cell(new Piece("k", "b", [0, 4]), "w"),
-        new Cell(new Piece("b", "b", [0, 5]), "b"),
-        new Cell(new Piece("n", "b", [0, 6]), "w"),
-        new Cell(new Piece("r", "b", [0, 7]), "b"),
-      ],
-      [
-        new Cell(new Piece("p", "b", [1, 0]), "b"),
-        new Cell(new Piece("p", "b", [1, 1]), "w"),
-        new Cell(new Piece("p", "b", [1, 2]), "b"),
-        new Cell(new Piece("p", "b", [1, 3]), "w"),
-        new Cell(new Piece("p", "b", [1, 4]), "b"),
-        new Cell(new Piece("p", "b", [1, 5]), "w"),
-        new Cell(new Piece("p", "b", [1, 6]), "b"),
-        new Cell(new Piece("p", "b", [1, 7]), "w"),
-      ],
-      [
-        new Cell(null, "w"),
-        new Cell(null, "b"),
-        new Cell(null, "w"),
-        new Cell(null, "b"),
-        new Cell(null, "w"),
-        new Cell(null, "b"),
-        new Cell(null, "w"),
-        new Cell(null, "b"),
-      ],
-      [
-        new Cell(null, "b"),
-        new Cell(null, "w"),
-        new Cell(null, "b"),
-        new Cell(null, "w"),
-        new Cell(null, "b"),
-        new Cell(null, "w"),
-        new Cell(null, "b"),
-        new Cell(null, "w"),
-      ],
-      [
-        new Cell(null, "w"),
-        new Cell(null, "b"),
-        new Cell(null, "w"),
-        new Cell(null, "b"),
-        new Cell(null, "w"),
-        new Cell(null, "b"),
-        new Cell(null, "w"),
-        new Cell(null, "b"),
-      ],
-      [
-        new Cell(null, "b"),
-        new Cell(null, "w"),
-        new Cell(null, "b"),
-        new Cell(null, "w"),
-        new Cell(null, "b"),
-        new Cell(null, "w"),
-        new Cell(null, "b"),
-        new Cell(null, "w"),
-      ],
-      [
-        new Cell(new Piece("p", "w", [6, 0]), "w"),
-        new Cell(new Piece("p", "w", [6, 1]), "b"),
-        new Cell(new Piece("p", "w", [6, 2]), "w"),
-        new Cell(new Piece("p", "w", [6, 3]), "b"),
-        new Cell(new Piece("p", "w", [6, 4]), "w"),
-        new Cell(new Piece("p", "w", [6, 5]), "b"),
-        new Cell(new Piece("p", "w", [6, 6]), "w"),
-        new Cell(new Piece("p", "w", [6, 7]), "b"),
-      ],
-      [
-        new Cell(new Piece("r", "w", [7, 0]), "b"),
-        new Cell(new Piece("n", "w", [7, 1]), "w"),
-        new Cell(new Piece("b", "w", [7, 2]), "b"),
-        new Cell(new Piece("q", "w", [7, 3]), "w"),
-        new Cell(new Piece("k", "w", [7, 4]), "b"),
-        new Cell(new Piece("b", "w", [7, 5]), "w"),
-        new Cell(new Piece("n", "w", [7, 6]), "b"),
-        new Cell(new Piece("r", "w", [7, 7]), "w"),
-      ],
-    ];
+
+    // Default board state if no parsedBoard is provided
+    this.state = makeDefaultState();
   }
 
   getBoardState() {
